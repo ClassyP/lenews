ed74361d714a60c52d8f9467b43a529330d3c419
classyp <jeanphilip@mecom>
2018-03-15 09:48:08 -0700

    update

--- server.js
+++ server.js
@@ -1,42 +1,48 @@
+// Web Scraper Homework Solution Example
+// (be sure to watch the video to see
+// how to operate the site in the browser)
+// -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+
+// Require our dependencies
 var express = require("express");
+var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
 
+// Set up our port to be either the host's designated port, or 3000
+var PORT = process.env.PORT || 3000;
 
-mongoose.Promise = Promise;
-
+// Instantiate our Express App
 var app = express();
-var PORT = process.env.PORT || 8000;
 
-// Use morgan and body parser with our app
-app.use(logger("dev"));
-app.use(bodyParser.urlencoded({extended: false}));
+// Require our routes
+var routes = require("./routes");
 
-// allow the handlesbars engine to be in our toolset
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-// Now set handlebars engine
-app.set('view engine', 'handlebars');
-
-// Make public a static dir to serve our static files
+// Designate our public folder as a static directory
 app.use(express.static("public"));
 
+// Connect Handlebars to our Express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// if any errors than console errors
-db.on("error", function (error) {
-  console.log("Mongoose Error: ", error);
+// Use bodyParser in our app
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Have every request go through our route middleware
+app.use(routes);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
 });
 
-// display a console message when mongoose has a conn to the db
-db.once("open", function () {
-  console.log("Mongoose connection successful.");
-});
-
-// Require the routes in our controllers js file
-require("./controllers/Articles.js")(app);
-
-
-app.listen(8000, function() {
-    console.log("App running on port 3000!");
+// Listen on the port
+app.listen(PORT, function() {
+  console.log("Listening on port: " + PORT);
 });
